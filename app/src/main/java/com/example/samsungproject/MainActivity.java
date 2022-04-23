b7b88f60368be64a33e0e6ea2e3e15aa21e9d62e
Tsanami <brajanmihajlov@gmail.com>
2022-04-23 23:21:18 +0800

    first commit

---
+++ app/src/main/java/com/example/samsungproject/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.samsungproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Game game;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        game = new Game(this);
+        setContentView(game);
+        Log.d("dd", "ddd");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+
+    @Override
+    protected void onPause() {
+        game.pause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+    }
+}
