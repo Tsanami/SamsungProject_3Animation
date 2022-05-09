57cf4865b2cf500f931cde78d635a8e27387eafc
Tsanami <brajanmihajlov@gmail.com>
2022-05-09 21:25:55 +0800

    animation optimized

--- app/src/main/java/com/example/samsungproject/Animator.java
+++ app/src/main/java/com/example/samsungproject/Animator.java
@@ -7,32 +7,37 @@ import android.graphics.Canvas;
 import android.graphics.Rect;
 
 public class Animator {
-    private static final int MAX_UPDATES_BEFORE_NEXT_MOVE = 5;
-    private final Player player;
-    private State state;
-    private Rect[] rect = new Rect[7];
+    private static final int MAX_SPRITES_PER_SECOND = 5;
     private int updatesBeforeNextMoveFrame;
-    private int idxNotMovingFrameR = 0;
-    private int idxNotMovingFrameL = 6;
-    private int idxMovingFrameR = 2; // move right
-    private int idxMovingFrameL = 4; // move left
-    private boolean dir; // direction of player
+
+    private final Player player;
     private static Bitmap bitmap;
+    private boolean dir; // direction of player
+
+
+    private State state;
+    private Rect standingR;
+    private Rect standingL;
+    private Rect[] moveR = new Rect[3];
+    private Rect[] moveL = new Rect[3];
+    private int idxR = 0;
+    private int idxL = 0;
+    private boolean isStanding = true;
 
     public Animator(Context context, Player player) {
         int width = 167;
         int height = 250;
         int distance = 9;
-        rect[0] = new Rect(0,0, width, height);
-        rect[1] = new Rect(width+distance, 0,width*2+distance, 250);
-        rect[2] = new Rect(width*2+distance*2,0,width*3+distance*2, 250);
-        rect[3] = new Rect(width*3+distance*3,0,width*4+distance*3 , 250);
-        rect[4] = new Rect(0,height+distance, width,height*2+distance);
-        rect[5] = new Rect(width+distance,height+distance, width*2+distance,height*2+distance);
-        rect[6] = new Rect(width*2+distance*2,height+distance, width*3+distance*2,height*2+distance);
 
+        standingR =  new Rect(0,0, width, height);
+        standingL = new Rect(width*2+distance*2,height+distance, width*3+distance*2,height*2+distance);
+        moveR[0] = new Rect(width+distance, 0,width*2+distance, 250);
+        moveR[1] = new Rect(width*2+distance*2,0,width*3+distance*2, 250);
+        moveR[2] = new Rect(width*3+distance*3,0,width*4+distance*3 , 250);
+        moveL[0] = new Rect(width*2+distance*2,height+distance, width*3+distance*2,height*2+distance);
+        moveL[1] = new Rect(0,height+distance, width,height*2+distance);
+        moveL[2] = new Rect(width+distance,height+distance, width*2+distance,height*2+distance);
         BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
-
         bitmapOptions.inScaled = false;
         bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.hero_sprites_test, bitmapOptions);
 
@@ -47,125 +52,71 @@ public class Animator {
     }
 
     public void draw(Canvas canvas) {
-
-//        switch (state){
-//            case NOT_MOVING:
-//                if (player.getVelX() != 0 || player.getVelY() != 0 ) {
-//                    state = State.STARTED_MOVING;
-//                }
-//                break;
-//            case STARTED_MOVING:
-//                if (player.getVelX() != 0 || player.getVelY() != 0 ) {
-//                    state = State.IS_MOVING;
-//                }
-//                break;
-//            case IS_MOVING:
-//                if (player.getVelX() == 0 && player.getVelY() == 0) {
-//                    state = State.NOT_MOVING;
-//                }
-//                break;
-//            default:
-//                break;
-//        }
-//
-//        switch (state){
-//            case NOT_MOVING:
-//                if (dir)
-//                    drawFrame(canvas, player, idxNotMovingFrameR);
-//                else
-//                    drawFrame(canvas, player, idxNotMovingFrameL);
-//                break;
-//            case STARTED_MOVING:
-//                updatesBeforeNextMoveFrame = MAX_UPDATES_BEFORE_NEXT_MOVE;
-//                if (player.getVelX() < 0)
-//                    drawFrame(canvas, player, idxMovingFrameL);
-//                else
-//                    drawFrame(canvas, player, idxMovingFrameR);
-//                break;
-//            case IS_MOVING:
-//                updatesBeforeNextMoveFrame--;
-//                dir = player.getVelX() > 0;
-//                if (updatesBeforeNextMoveFrame==0){
-//                    updatesBeforeNextMoveFrame = MAX_UPDATES_BEFORE_NEXT_MOVE;
-//                    toggleIdxMovingFrame(dir);
-//                }
-//                if (player.getVelX() > 0)
-//                    drawFrame(canvas, player, idxMovingFrameR);
-//                else
-//                    drawFrame(canvas, player, idxMovingFrameL);
-//                break;
-//        }
+        float pVX = player.getVelX();
+        float pVY = player.getVelY();
+        isStanding = pVX==0;
+        whichState(pVX, pVY);
         switch (state){
             case NOT_MOVING:
-                if (player.getVelX() != 0 || player.getVelY() != 0 ) {
-                    state = State.STARTED_MOVING;
-                    break;
-                }
+                drawSprite(canvas, player, isStanding);
                 break;
             case STARTED_MOVING:
-                if (player.getVelX() != 0 || player.getVelY() != 0 ) {
-                    state = State.IS_MOVING;
-                    break;
-                }
-                break;
-            case IS_MOVING:
-                if (player.getVelX() == 0 && player.getVelY() == 0) {
-                    state = State.NOT_MOVING;
-                    break;
-                }
-                break;
-            default:
-                break;
-        }
-
-        switch (state){
-            case NOT_MOVING:
-                if (dir)
-                    drawFrame(canvas, player, idxNotMovingFrameR);
-                else
-                    drawFrame(canvas, player, idxNotMovingFrameL);
-                break;
-            case STARTED_MOVING:
-                updatesBeforeNextMoveFrame = MAX_UPDATES_BEFORE_NEXT_MOVE;
-                if (player.getVelX() < 0)
-                    drawFrame(canvas, player, idxMovingFrameL);
-                else
-                    drawFrame(canvas, player, idxMovingFrameR);
+                updatesBeforeNextMoveFrame = MAX_SPRITES_PER_SECOND;
+                drawSprite(canvas, player, isStanding);
                 break;
             case IS_MOVING:
                 updatesBeforeNextMoveFrame--;
-                dir = player.getVelX() > 0;
+                dir = pVX > 0;
                 if (updatesBeforeNextMoveFrame==0){
-                    updatesBeforeNextMoveFrame = MAX_UPDATES_BEFORE_NEXT_MOVE;
-                    toggleIdxMovingFrame(dir);
+                    updatesBeforeNextMoveFrame = MAX_SPRITES_PER_SECOND;
+                    if (dir) {
+                        if (idxR >= 2)
+                            idxR = 0;
+                        idxR++;
+                    }
+                    else {
+                        if (idxL >= 2)
+                            idxL = 0;
+                        idxL++;
+                    }
                 }
-                if (player.getVelX() > 0)
-                    drawFrame(canvas, player, idxMovingFrameR);
-                else
-                    drawFrame(canvas, player, idxMovingFrameL);
+                drawSprite(canvas, player, isStanding);
                 break;
         }
     }
 
-    private void toggleIdxMovingFrame(boolean dir) {
-        if (dir) {
-            if (idxMovingFrameR == 2)
-                idxMovingFrameR = 3;
-            else
-                idxMovingFrameR = 2;
+    private void whichState(float pVX, float pVY){
+        if (state == State.NOT_MOVING && (pVX != 0 || pVY != 0)) {
+            state = State.STARTED_MOVING;
+            isStanding = false;
         }
-        else{
-            if (idxMovingFrameL == 4)
-                idxMovingFrameL = 5;
-            else
-                idxMovingFrameL = 4;
+        else if (state == State.STARTED_MOVING && (pVX != 0 || pVY != 0 )) {
+            state = State.IS_MOVING;
+            isStanding = false;
+        }
+        else if (state == State.IS_MOVING && ((pVX == 0 && pVY == 0))) {
+            state = State.NOT_MOVING;
+            isStanding = true;
         }
     }
-
-    public void drawFrame(Canvas canvas, Player player, int ind) {
+    public void drawSprite(Canvas canvas, Player player, boolean isStanding) {
         int playerX = (int)player.getPosX();
         int playerY = (int)player.getPosY();
-        canvas.drawBitmap(bitmap, rect[ind], new Rect(playerX, playerY, playerX+167, playerY+250), null);
+        Rect sRect = new Rect(playerX, playerY, playerX+167, playerY+250);
+
+        if (isStanding) {
+            if (dir)
+                canvas.drawBitmap(bitmap, standingR, sRect, null);
+            else
+                canvas.drawBitmap(bitmap, standingL, sRect, null);
+        }
+        else {
+            if (dir) {
+                canvas.drawBitmap(bitmap, moveR[idxR], sRect, null);
+            } else {
+                canvas.drawBitmap(bitmap, moveL[idxL], sRect, null);
+            }
+        }
     }
 
 }
